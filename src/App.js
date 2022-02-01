d1c2eb96cc4dd37567e8805e7217f2ad1309b7ea
Januario86 <januario86fp@gmail.com>
2022-02-01 20:47:53 -0300

    feat:imc estrutura de tela

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ export default function App(){
       <h1>Calculadora IMC</h1>
       <span>Vamos Calcular seu imc</span>
 
+      <div className="area-input">
+        <input 
+            type="text"
+            placeholder="Peso em (Kg) Ex:90"        
+        />
+        <input 
+            type="text"
+            placeholder="Altura em (cm) Ex:180"        
+        />
+
+        <button>
+          Calcular
+        </button>
+
+      </div>
+
+      <h2>Seu imc foi 25, você está abaixo do peso!</h2>
 
 
     </div>
